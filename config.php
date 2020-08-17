9b30f260c2e828ff733d503786e5a6165dda4e01
Jerrycyy <57795110+Jerrycyy@users.noreply.github.com>
2020-08-17 20:53:03 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php $configs = '
+{
+    "Onedrive": {
+        "Drive_ver": "CN",
+        "diskname": "JUU0JUI4JTk2JUU3JUJBJUFBJUU0JUJBJTkyJUU4JTgxJTk0T25kcml2ZSVFRiVCQyU4OCVFNSU5QiVCRCVFNSU4NiU4NSVFOCVBRSVCRiVFOSU5NyVBRSVFNiU4RSVBOCVFOCU4RCU5MCVFRiVCQyU4OQ",
+        "refresh_token": "OAQABAAAAAAAs2RXPF2swRJpJRjRbIf8xF-seRmXhXsgXOLJsTStLrwF_ANh-F2EDYh7zVcKFrGc4TM5B3b-xNICpk9s_DbEHaJ35qwDkczIk0jam3bKAHvxtbidKYxJQIJ577RTUI2ocMOrvxJ1fNvmBW7KYfepDV0W7v-kqNFVukj2oHZ2-sJhsT4r6FxpR-MSTGoiCnceS9wEb3JEdSPUg4r6wt4nAcAwUNhbQkNKkiOBR0--klWHwxO2DDGRLb0OBG-h3qs2-An8Lv1Q1XQfFWdxEOKRqPG7WwUF_Y97BYgZwrwvV2zt84KPbMwia5zckwypkqmBAXiZUDFpSdK9UTF2NrAl02G_1Mbh3108RtE6Dy9PyjvyMEphpdho7Yt6srFZW4FHQgbyU5B6hygNGHMoBuU3Pyhc1GjnubsbuzDLm47SyxqraCG1VzeOhzQTpedz8Zksfn8kuQ3CAcTnnzujf-r4C1Wsyq2Mp-l8MaTzwIY59N3p3leRwzjwYQJelay4xfIQQsqtStBz9A61TD33vi8B0xDoot7WFu33-o6eHaMidqxtNm3nOIS_1EZlpbLQuQuggPNmTcokoojhQ0wTEVzUDfFfwubY0uhhxT5s3TfdwbmY-5AOIPxpKKF0CY9ImbbIw2Nbs8DBGfMF8QPkk0egdBBPL6KvlQGBF-gXJfYQFHw8YHTrXmpk3IptWOt014o_KaI23XZvnAx31TtUaJB6QlNRZrzc2SKwfAnhjQzFO0kRlvTjTU03eJ03aRnb_dQAPngUx6aLspjHvVCYJIWaO8ZD8DEcJA4eEZUuDvcB9Xh6xYN-X7fKnsNl7d9kchTA-DoegPtqAKtKPsunW7OdDdiz6H9V5s_Bs8upvIuH9cXampbzbKr0KpLs9lhU76JkgAA",
+        "token_expires": 1598178887
+    },
+    "admin": "DHCa4805",
+    "disktag": "Onedrive",
+    "sitename": "SmVycnljeXklRTUlQjAlOEYlRTclQTAlQjQlRTclQkQlOTElRTclOUIlOTg",
+    "theme": "minus.php",
+    "timezone": "8"
+}
+';
